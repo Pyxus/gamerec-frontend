45b62ad3876f575464fd039f24906acbefa6f56d
pyxus <eze.halley@gmail.com>
2023-12-13 00:53:00 -0500

    Change GameSelect to use Combobox

---
+++ src/components/GameSelect/index.tsx
@@ -0,0 +1,131 @@
+import {
+  Combobox,
+  TextInput,
+  useCombobox,
+  Button,
+  NumberInput,
+} from "@mantine/core";
+import "./index.css";
+import { GameSearchResult } from "@/types";
+
+interface GameSelectProps {
+  isSearchingDatabase: boolean;
+  selectedGameName: string;
+  gameSearchResults: GameSearchResult[];
+  onGameRemoved: () => void;
+  onGameRatingChanged: (newRating: number) => void;
+  onGameSelected: (gameName: string) => void;
+  onGameSearchTextChanged: (newSearchText: string) => void;
+}
+
+function GameSelect({
+  isSearchingDatabase,
+  onGameRemoved,
+  onGameRatingChanged,
+  onGameSelected,
+  onGameSearchTextChanged,
+  selectedGameName,
+  gameSearchResults,
+}: GameSelectProps) {
+  const combobox = useCombobox({
+    onDropdownClose: () => combobox.resetSelectedOption(),
+  });
+
+  const options = isSearchingDatabase
+    ? SearchingForGameOption(selectedGameName)
+    : FilteredGameOptions(gameSearchResults, selectedGameName);
+
+  return (
+    <Combobox
+      onOptionSubmit={(optionValue) => {
+        if (!isSearchingDatabase) {
+          onGameSelected(optionValue);
+          combobox.closeDropdown();
+        }
+      }}
+      store={combobox}
+    >
+      <Combobox.Target>
+        <div className="selection-list">
+          <Button
+            className="remove-button"
+            variant="light"
+            onClick={onGameRemoved}
+          >
+            X
+          </Button>
+          <TextInput
+            w={"100%"}
+            placeholder="Start typing a game title..."
+            value={selectedGameName}
+            onChange={(event) => {
+              const gameSearchText = event.currentTarget.value;
+
+              if (!isSearchingDatabase) combobox.selectFirstOption();
+
+              onGameSearchTextChanged(gameSearchText);
+
+              gameSearchText.length > 0
+                ? combobox.openDropdown()
+                : combobox.closeDropdown();
+            }}
+            onClick={() => {
+              if (selectedGameName.length > 0) combobox.openDropdown();
+            }}
+            onFocus={() => {
+              if (selectedGameName.length > 0) combobox.openDropdown();
+            }}
+            onBlur={() => combobox.closeDropdown()}
+          />
+          <NumberInput
+            defaultValue={1}
+            pl={5}
+            w={"15%"}
+            onChange={(value) => onGameRatingChanged(Number(value))}
+          />
+        </div>
+      </Combobox.Target>
+
+      <Combobox.Dropdown>
+        <Combobox.Options>{options}</Combobox.Options>
+      </Combobox.Dropdown>
+    </Combobox>
+  );
+}
+
+function SearchingForGameOption(gameName: string) {
+  return (
+    <Combobox.Option value={`Searching database for ${gameName}...`} key={-1}>
+      {"Searching database for "}
+      <b>{`${gameName}...`}</b>
+    </Combobox.Option>
+  );
+}
+
+function FilteredGameOptions(
+  games: GameSearchResult[],
+  selectedGameName: string
+) {
+  const shouldFilterOptions = !games.some(
+    (item) => item.name === selectedGameName
+  );
+
+  const filteredOptions = shouldFilterOptions
+    ? games.filter((item) =>
+        item.name.toLowerCase().includes(selectedGameName.toLowerCase().trim())
+      )
+    : games;
+
+  if (filteredOptions.length === 0) {
+    return [<Combobox.Empty>Nothing found</Combobox.Empty>];
+  } else {
+    return filteredOptions.map((item, index) => (
+      <Combobox.Option value={item.name} key={index}>
+        {item.name}
+        {` (${new Date(item.first_release_date * 1000).getFullYear()})`}
+      </Combobox.Option>
+    ));
+  }
+}
+
+export default GameSelect;
