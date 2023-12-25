5d8a485b4ce5c7aae1ad2f54349b5beebf5bd2a7
pyxus <eze.halley@gmail.com>
2023-12-25 00:36:35 -0500

    Add warning when attempt to close last input game

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import '@mantine/core/styles.css';
-import { MantineProvider, createTheme } from '@mantine/core';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "@mantine/core/styles.css";
+import { MantineProvider, createTheme } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 
 const theme = createTheme({
   /** Put your mantine theme override here */
 });
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MantineProvider theme={theme}>
+      <Notifications zIndex={1000} />
       <App />
     </MantineProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
